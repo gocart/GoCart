19affb9d2ab505e1c7472fb966b4e776f111b4c2
Noah Mormino <Programmer@imac-2.local>
2011-11-07 09:02:18 -0600

    Force_SSL bug fix
    Force_SSL was trying to force SSL even if SSL isn't enabled.

--- gocart/helpers/MY_url_helper.php
+++ gocart/helpers/MY_url_helper.php
@@ -23,11 +23,12 @@ if ( ! function_exists('force_ssl'))
 if ( ! function_exists('remove_ssl'))
 {
 	function remove_ssl()
-	{
-		$CI =& get_instance();
-		$CI->config->config['base_url'] = str_replace('https://', 'http://', $CI->config->config['base_url']);
+	{	
 		if ($_SERVER['SERVER_PORT'] != 80 || (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on'))
 		{
+			$CI =& get_instance();
+			$CI->config->config['base_url'] = str_replace('https://', 'http://', $CI->config->config['base_url']);
+			
 			redirect($CI->uri->uri_string());
 		}
 	}
