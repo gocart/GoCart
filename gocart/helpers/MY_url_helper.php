21e7d3fb20f1b41f2c5de6605cd312b8e6c7ef20
Noah Mormino <Programmer@imac-2.local>
2011-11-08 09:52:36 -0600

    SSL URL fix
    I meant to do this before and obviously missed it :(

--- gocart/helpers/MY_url_helper.php
+++ gocart/helpers/MY_url_helper.php
@@ -10,10 +10,10 @@ if ( ! function_exists('force_ssl'))
 {
 	function force_ssl()
 	{
-		$CI =& get_instance();
-		$CI->config->config['base_url'] = str_replace('http://', 'https://', $CI->config->config['base_url']);
 		if (ssl_support() &&  (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == 'off'))
 		{
+			$CI =& get_instance();
+			$CI->config->config['base_url'] = str_replace('http://', 'https://', $CI->config->config['base_url']);
 			redirect($CI->uri->uri_string());
 		}
 	}
