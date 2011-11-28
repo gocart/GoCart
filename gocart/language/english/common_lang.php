2f1375975f4fd710637eb5a0dc70f62771328569
Noah Mormino <Programmer@imac-2.local>
2011-11-28 15:16:12 -0600

    additional changes to support language library

--- gocart/language/english/common_lang.php
+++ gocart/language/english/common_lang.php
@@ -7,33 +7,52 @@ Common Language
 
 ******************************************/
 
-//common terms
 $lang['login']				= 'Login';
 $lang['logout']				= 'Logout';
 $lang['my_account']			= 'My Account';
-
 
 $lang['empty_cart']			= 'Your cart is empty';
 $lang['single_item']		= 'There is %d items in your cart';
 $lang['multiple_items']		= 'There are %d items in your cart';
 
-//form elements
-$lang['form_submit']		= "Submit";
-$lang['form_search']		= "Search";
-$lang['form_cancel']		= "Cancel";
+$lang['form_submit']		= 'Submit';
+$lang['form_register']		= 'Register';
+$lang['form_login']			= 'Login';
+$lang['form_search']		= 'Search';
+$lang['form_cancel']		= 'Cancel';
 $lang['form_add_to_cart']	= 'Add to Cart';
+$lang['form_update_cart']	= 'Update Cart';
+$lang['form_checkout']		= 'Checkout &raquo;';
 $lang['form_continue']		= 'Continue';
 $lang['form_choose']		= 'Choose';
 $lang['form_edit']			= 'Edit';
 $lang['form_delete']		= 'Delete';
 
-//catalog
-$lang['products']			= 'Products';
-$lang['out_of_stock']		= 'Out of Stock';
-$lang['no_products']		= 'There are currently no available products in this category.';
-$lang['no_image_available']	= 'No Image Available';
+$lang['search']				= 'Search';
+$lang['amount']				= 'Amount';
+$lang['custom_amount']		= 'Custom Amount';
+$lang['preset_amount']		= 'Preset Amount';
+$lang['recipient_name']		= 'Recipient Name';
+$lang['recipient_email']	= 'Recipient Email';
+$lang['sender_email']		= 'Sender Email';
+$lang['custom_greeting']	= 'Custom Greeting Message';
+$lang['check_out']			= 'Check Out';
 
-//checkout
+$lang['products']				= 'Products';
+$lang['out_of_stock']			= 'Out of Stock';
+$lang['no_products']			= 'There are currently no available products in this category.';
+$lang['no_image_available']		= 'No Image Available';
+$lang['product_price']			= 'Price:';
+$lang['product_sale']			= 'SALE:';
+$lang['available_options']		= 'Available Options';
+$lang['choose_option']			= 'Choose an Option';
+$lang['tab_description']		= 'Description';
+$lang['tab_related_products']	= 'Related Products';
+
+$lang['coupon_label']				= 'If you have a coupon, enter the code here:';
+$lang['gift_card_label']			= 'If you have a Gift Certificate, enter the code here:';
+$lang['apply_gift_card']			= 'Apply Gift Card';
+$lang['apply_coupon']				= 'Apply Coupon';
 $lang['customer_information']		= 'Customer Information';
 $lang['error_save_payment']			= 'There was a problem saving your payment method';
 $lang['error_choose_payment']		= 'Please choose a payment method';
@@ -48,13 +67,13 @@ $lang['choose_payment_method']		= 'Choose Payment Method';
 $lang['shipping_method']			= 'Shipping Method';
 $lang['no_shipping_needed']			= 'Your order does not include any items that require shipping.';
 $lang['payment_information']		= 'Payment Information';
-
-//checkout_summary
-$lang['product_information']		= 'Product Info'; /* this is used in the email confirmation */
-$lang['price_and_quantity']			= 'Price &amp; Quantity'; /* this is used in the email confirmation */
+$lang['payment_method']				= 'Payment Method';
+$lang['submit_payment_method']		= 'Submit Payment Method';
+$lang['product_information']		= 'Product Info';
+$lang['price_and_quantity']			= 'Price &amp; Quantity';
 $lang['sku']						= 'SKU';
 $lang['name']						= 'Name';
-$lang['Price']						= 'Price';
+$lang['price']						= 'Price';
 $lang['description']				= 'Description';
 $lang['quantity']					= 'Quantity';
 $lang['totals']						= 'Totals';
@@ -67,7 +86,6 @@ $lang['tax']						= 'Tax';
 $lang['taxes']						= 'Taxes';
 $lang['grand_total']				= 'Grand Total';
 
-//addresses
 $lang['address_manager']			= 'Address Manager';
 $lang['your_addresses']				= 'Your Addresses';
 $lang['shipping_and_billing']		= 'Shipping &amp; Billing Address';
@@ -77,6 +95,7 @@ $lang['use_address_for_billing']	= 'Use this address for billing';
 $lang['shipping_address']			= 'Shipping Address';
 $lang['choose_shipping_address']	= 'Choose Your Shipping Address';
 $lang['choose_address']				= 'Choose Address';
+$lang['ship_to_address']			= 'Ship to this address';
 $lang['address_company']			= 'Company';
 $lang['address']					= 'Address';
 $lang['address_firstname']			= 'First Name';
@@ -87,13 +106,13 @@ $lang['address_city']				= 'City';
 $lang['address_postcode']			= 'Zip / Post Code';
 $lang['address_country']			= 'Country';
 $lang['address_state']				= 'State';
+$lang['message_address_saved']		= 'Your address has been saved!';
 
-//checkout - Additional Details
 $lang['additional_order_details']	= 'Additional Order Details';
+$lang['additional_details']			= 'Additional Details';
 $lang['heard_about']				= 'How did you hear about us?';
 $lang['shipping_instructions']		= 'Shipping Instructions?';
 
-//giftcards
 $lang['giftcard']					= 'Gift Card';
 $lang['giftcard_choose_amount']		= 'Choose Gift Card Amount';
 $lang['giftcard_custom_amount']		= 'Custom Amount';
@@ -101,16 +120,24 @@ $lang['giftcard_to']				= 'To (Name)';
 $lang['giftcard_email']				= 'To (Email)';
 $lang['giftcard_from']				= 'From';
 $lang['giftcard_message']			= 'Personal Message';
+$lang['giftcard_excerpt']			= 'Gift Card for a friend<br/>To: %s';
 
-//login and forgot password
 $lang['email']						= 'Email';
 $lang['password']					= 'Password';
 $lang['return_to_login']			= 'Return to Login';
 $lang['forgot_password']			= 'Forgot Password';
 $lang['keep_me_logged_in']			= 'Keep me logged in!';
 $lang['register']					= 'Register';
+$lang['login_failed']				= 'Authentication Failed!';
+$lang['go_to_login']				= 'Go to Login';
 
-//my account
+$lang['account_registration']			= 'Account Registration';
+$lang['registration_thanks']			= 'thanks for registering %s!';
+$lang['error_email']					= 'The requested email is already in use.';
+$lang['error_no_account_record']		= 'There is no record of your account.';
+$lang['message_new_password']			= 'A new password has been generated and sent to your email.';
+$lang['message_account_updated']		= 'Your account has been updated';
+
 $lang['delete_address_confirmation']	= 'Are you sure you want to delete this address?';
 $lang['error_must_have_address']		= 'You Must leave at least 1 address in the Address Manager.';
 $lang['account_information']			= 'Account Information';
@@ -129,6 +156,7 @@ $lang['order_history']					= 'Order History';
 $lang['order_date']						= 'Ordered On';
 $lang['order_number']					= 'Order Number';
 $lang['order_status']					= 'Status';
+$lang['add_address']					= 'Add Address';
 $lang['no_order_history']				= 'You have no orders in your account history.';
 
 
