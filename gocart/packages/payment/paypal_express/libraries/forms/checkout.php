144bfe9575c4bf577486e1a8a269f6712b189ca5
Clear Sky Designs <gabe@clearskydesignscom>
2011-11-29 21:07:21 -0600

    Revert "Finished converting payment and shipping libs to package format, moved language content"
    This reverts commit 71dbd956cd4c939c90ecd1869251d2082239920d.

--- gocart/packages/payment/paypal_express/libraries/forms/checkout.php
+++ gocart/packages/payment/paypal_express/libraries/forms/checkout.php
@@ -3,6 +3,6 @@
     <td><img  src="https://www.paypal.com/en_US/i/logo/PayPal_mark_180x113.gif" border="0" alt="Acceptance Mark"></td>
   </tr>
   <tr>
-    <td><?php echo lang('paypal_desc') ?></td>
+    <td>Using this method, you will be directed to the Paypal website to verify your payment. Once your payment is authorized, you will be directed back to our website and your order will be complete.</td>
   </tr>
 </table>
