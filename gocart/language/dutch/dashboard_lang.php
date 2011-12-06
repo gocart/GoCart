84c78e5bbd580dc07576d7c78b89780f1ab3b91b
Pieter Verhaeghe <pieter@verhaeghe.be>
2011-12-06 14:38:07 +0100

    Just translated another file to Dutch

---
+++ gocart/language/dutch/dashboard_lang.php
@@ -0,0 +1,18 @@
+<?php
+/******************************************
+NL Dutch
+Admin Dashboard Language
+******************************************/
+
+$lang['dashboard']					= 'Controlepaneel';
+$lang['order_number']				= 'Nummer bestelling';
+$lang['bill_to']					= 'Facturatieadres';
+$lang['ship_to']					= 'Leveringsadres';
+$lang['ordered_on']					= 'Besteld op';
+$lang['status']						= 'Status';
+$lang['firstname']					= 'Voornaam';
+$lang['lastname']					= 'Naam';
+$lang['email']						= 'E-mail';
+$lang['active']						= 'Actief';
+$lang['yes']						= 'Ja';
+$lang['no']							= 'Nee';
