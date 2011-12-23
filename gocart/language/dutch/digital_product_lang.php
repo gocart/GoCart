1a26b567b2032ec1ee464ec28166b6a27ae195bc
Pieter Verhaeghe <pieter@verhaeghe.be>
2011-12-23 10:03:09 +0100

    Dutch language update to reflect new features: digital products and inventory control

---
+++ gocart/language/dutch/digital_product_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['dgtl_pr_header'] = 'Bestanden digitale producten';
+
+$lang['confirm_delete_file']	= 'Opgepast: Een bestand wissen dat is gekoppeld aan een actief product zal leiden tot onbeschikbaarheid van het digitale product.';
+$lang['path_error']				= 'Het map voor uploads van bestanden is niet schrijfbaar.  Controleer de rechten op deze map vooraleer bestanden te uploaden.';
+$lang['message_deleted_file']	= 'Het bestand is gewist.';
+
+$lang['add_file']				= 'Bestand toevoegen';
+$lang['digital_products_form']	= 'Formulier digitale producten';
+$lang['file_label']				= 'Selecteer bestand';
+$lang['version']				= 'Versie';
+$lang['max_downloads']			= 'Max downloads';
+$lang['max_downloads_note']		= '(Max downloads per aankoop - 0 voor ongelimiteerd)';
+$lang['filename']				= 'Bestandsnaam';
+$lang['title']					= 'Titel';
+$lang['size']					= 'Bestandsgrootte';
+$lang['avail']					= 'Beschikbaar';
+$lang['no_files']				= 'Er zijn geen bestanden';
+
+$lang['download_limit']			= 'Resterende downloads';
+$lang['no_downloads']			= 'Er zijn geen downloads beschikbaar.';
+$lang['no_max']					= 'Nvt';
+$lang['max_exceeded']			= 'Max downloads bereikt';
+
+$lang['download_btn']			= 'Download';
+	
+$lang['attributes']				= 'Attributen';
+
+$lang['missing_file_msg']		= 'Oeps! Het bestand is niet beschikbaar om te downloaden op dit moment. We proberen dit zo snel mogelijk op te lossen.';
