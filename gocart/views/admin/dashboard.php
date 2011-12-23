cbcb4f716b91489f124d039c5ff6ebc219c5c335
Noah Mormino <Programmer@Noahs-iMac.local>
2011-12-23 11:29:32 -0600

    Added shipping and payment module notification to dashboard
    This change notifies administrators when they have no shipping and
    payment modules installed. This does not check if those modules are
    enabled (since the enabled flag isn't a required feature of a module)
    or functional. It only checks if they are references to them in the
    database.

--- gocart/views/admin/dashboard.php
+++ gocart/views/admin/dashboard.php
@@ -1,5 +1,19 @@
 <?php include('header.php'); ?>
 
+<?php if(!$payment_module_installed):?>
+<div class="ui-state-highlight ui-corner-all" style="padding:10px; margin-bottom:10px;"> 
+	<p><span class="ui-icon ui-icon-info" style="float: left; margin-right: .3em;"></span>
+	<strong><?php echo lang('common_note') ?>:</strong> <?php echo lang('no_payment_module_installed'); ?></p>
+</div>
+<?php endif;?>
+
+<?php if(!$shipping_module_installed):?>
+<div class="ui-state-highlight ui-corner-all" style="padding:10px; margin-bottom:10px;"> 
+	<p><span class="ui-icon ui-icon-info" style="float: left; margin-right: .3em;"></span>
+	<strong><?php echo lang('common_note') ?>:</strong> <?php echo lang('no_shipping_module_installed'); ?></p>
+</div>
+<?php endif;?>
+
 <h3><?php echo lang('recent_orders') ?></h3>
 <table class="gc_table" cellspacing="0" cellpadding="0">
     <thead>
