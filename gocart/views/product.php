b99ce9385eaee791725513af22c4200cb6a7717e
Noah Mormino <Programmer@Noahs-iMac.local>
2012-01-02 14:22:04 -0600

    fixed no image available code for related products

--- gocart/views/product.php
+++ gocart/views/product.php
@@ -215,8 +215,10 @@
 				<div class="category_box">
 					<div class="thumbnail">
 						<?php
+						$photo	= '<img src="'.base_url('images/nopicture.png').'" alt="'.lang('no_image_available').'"/>';
 						$product->images	= array_values($product->images);
-						if(count($product->images) > 0)
+
+						if(!empty($product->images[0]))
 						{
 							$primary	= $product->images[0];
 							foreach($product->images as $photo)
