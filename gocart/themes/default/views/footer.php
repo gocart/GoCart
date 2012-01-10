5030dd2d7a5572cced2ba74d136df3d31d3802ba
Noah Mormino <Programmer@Noahs-iMac.local>
2012-01-09 20:26:26 -0600

    updated add-this code to remove https variable need

--- gocart/themes/default/views/footer.php
+++ gocart/themes/default/views/footer.php
@@ -16,7 +16,7 @@
 		<a class="addthis_button_compact"></a>
 		<a class="addthis_counter addthis_bubble_style"></a>
 		</div>
-		<script type="text/javascript" src="<?php echo $http;?>://s7.addthis.com/js/250/addthis_widget.js#pubid=xa-4e37144a288bd489"></script>
+		<script type="text/javascript" src="https://s7.addthis.com/js/250/addthis_widget.js#pubid=xa-4e37144a288bd489"></script>
 		<!-- AddThis Button END -->
 	</div>
 	
