985bda98cd4576a2f5aa74e978eeb9f3fa32b36b
Noah Mormino <Programmer@Noahs-iMac.local>
2012-04-12 10:20:37 -0500

    Fixed the date fields to properly save on boxes

--- gocart/views/admin/box_form.php
+++ gocart/views/admin/box_form.php
@@ -3,8 +3,8 @@
 <?php
 
 $title			= array('class'=>'gc_tf1', 'name'=>'title', 'id'=>'title', 'value' => set_value('title', $title));
-$enable_on		= array('class'=>'gc_tf1', 'id'=>'enable_on', 'value' => set_value('enable_on', reverse_format($enable_on)));
-$disable_on		= array('class'=>'gc_tf1', 'id'=>'disable_on', 'value' => set_value('disable_on', reverse_format($disable_on)));
+$enable_on_alt	= array('class'=>'gc_tf1', 'id'=>'enable_on_alt', 'value' => set_value('enable_on', reverse_format($enable_on)));
+$disable_on_alt	= array('class'=>'gc_tf1', 'id'=>'disable_on_alt', 'value' => set_value('disable_on', reverse_format($disable_on)));
 $f_image		= array('style'=>'pboxding:5px','name'=>'image', 'id'=>'image');
 $link			= array('class'=>'gc_tf1', 'name'=>'link', 'id'=>'link', 'value' => set_value('link', $link));	
 $new_window		= array('id'=>'new_window', 'name'=>'new_window', 'value'=>1, 'checked'=>set_checkbox('new_window', 1, $new_window));
@@ -33,14 +33,14 @@ $new_window		= array('id'=>'new_window', 'name'=>'new_window', 'value'=>1, 'chec
 
 		<div class="gc_field2">
 			<label for="enable_on"><?php echo lang('enable_on');?> </label>
-			<?php echo form_input($enable_on); ?>
-			<input type="hidden" name="enable_on_alt" id="enable_on_alt" value="<?php echo set_value('enable_on', $enable_on) ?>" />
+			<?php echo form_input($enable_on_alt); ?>
+			<input type="hidden" name="enable_on" id="enable_on" value="<?php echo set_value('enable_on', $enable_on) ?>" />
 		</div>
 
 		<div class="gc_field2">
 			<label for="disable_on"><?php echo lang('disable_on');?> </label>
-			<?php echo form_input($disable_on); ?>
-			<input type="hidden" name="disable_on_alt" id="disable_on_alt" value="<?php echo set_value('disable_on', $disable_on) ?>" />
+			<?php echo form_input($disable_on_alt); ?>
+			<input type="hidden" name="disable_on" id="disable_on" value="<?php echo set_value('disable_on', $disable_on) ?>" />
 		</div>
 
 		<div class="gc_field2">
@@ -61,8 +61,8 @@ $new_window		= array('id'=>'new_window', 'name'=>'new_window', 'value'=>1, 'chec
 </form>
 <script type="text/javascript">
 	$(document).ready(function() {
-		$("#enable_on").datepicker({ dateFormat: 'mm-dd-yy', altField: '#enable_on_alt', altFormat: 'yy-mm-dd' });
-		$("#disable_on").datepicker({ dateFormat: 'mm-dd-yy', altField: '#disable_on_alt', altFormat: 'yy-mm-dd' });
+		$("#enable_on_alt").datepicker({ dateFormat: 'mm-dd-yy', altField: '#enable_on', altFormat: 'yy-mm-dd' });
+		$("#disable_on_alt").datepicker({ dateFormat: 'mm-dd-yy', altField: '#disable_on', altFormat: 'yy-mm-dd' });
 	});
 </script>
 <?php include('footer.php'); ?>
