18f3e5545ec8ec5070bf41cb4bc77c2fb6e5a1e6
Noah Mormino <Programmer@Noahs-iMac.local>
2012-07-12 16:43:47 -0500

    updated customer pagination config.

--- gocart/controllers/admin/customers.php
+++ gocart/controllers/admin/customers.php
@@ -28,7 +28,29 @@ class Customers extends Admin_Controller {
 		$config['total_rows']	= $this->Customer_model->count_customers();
 		$config['per_page']		= 50;
 		$config['uri_segment']	= 6;
+		$config['first_link']		= 'First';
+		$config['first_tag_open']	= '<li>';
+		$config['first_tag_close']	= '</li>';
+		$config['last_link']		= 'Last';
+		$config['last_tag_open']	= '<li>';
+		$config['last_tag_close']	= '</li>';
 
+		$config['full_tag_open']	= '<div class="pagination"><ul>';
+		$config['full_tag_close']	= '</ul></div>';
+		$config['cur_tag_open']		= '<li class="active"><a href="#">';
+		$config['cur_tag_close']	= '</a></li>';
+		
+		$config['num_tag_open']		= '<li>';
+		$config['num_tag_close']	= '</li>';
+		
+		$config['prev_link']		= '&laquo;';
+		$config['prev_tag_open']	= '<li>';
+		$config['prev_tag_close']	= '</li>';
+
+		$config['next_link']		= '&raquo;';
+		$config['next_tag_open']	= '<li>';
+		$config['next_tag_close']	= '</li>';
+		
 		$this->pagination->initialize($config);
 		
 		
