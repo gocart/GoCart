0021b304ebad08c6e9837d6ea834d1c379b73614
Noah Mormino <noah@noahmormino.com>
2012-09-17 13:35:37 -0500

    Updated MY_Controller to take care of most of the constructor needs.

--- gocart/controllers/cart.php
+++ gocart/controllers/cart.php
@@ -1,19 +1,6 @@
 <?php
 
-class Cart extends CI_Controller {
-	
-	//we collect the categories automatically with each load rather than for each function
-	//this just cuts the codebase down a bit
-	var $categories	= '';
-	
-	//load all the pages into this variable so we can call it from all the methods
-	var $pages = '';
-	
-	// determine whether to display gift card link on all cart pages
-	//  This is Not the place to enable gift cards. It is a setting that is loaded during instantiation.
-	var $gift_cards_enabled; 
-	
-	var $header_text;
+class Cart extends Front_Controller {
 
 	function __construct()
 	{
@@ -21,29 +8,6 @@ class Cart extends CI_Controller {
 		
 		//make sure we're not always behind ssl
 		remove_ssl();
-		
-		$this->load->library('Go_cart');
-		$this->load->model(array('Page_model', 'Product_model', 'Digital_Product_model', 'Gift_card_model', 'Option_model', 'Order_model', 'Settings_model'));
-		$this->load->helper(array('form_helper', 'formatting_helper'));
-		
-		//fill in our variables
-		$this->categories	= $this->Category_model->get_categories_tierd(0);
-		$this->pages		= $this->Page_model->get_pages();
-		
-		// check if giftcards are enabled
-		$gc_setting = $this->Settings_model->get_settings('gift_cards');
-		if(!empty($gc_setting['enabled']) && $gc_setting['enabled']==1)
-		{
-			$this->gift_cards_enabled = true;
-		}			
-		else
-		{
-			$this->gift_cards_enabled = false;
-		}
-		
-		//load the theme package
-		$this->load->add_package_path(APPPATH.'themes/'.$this->config->item('theme').'/');
-		
 	}
 
 	function index()
--- gocart/controllers/secure.php
+++ gocart/controllers/secure.php
@@ -1,21 +1,6 @@
 <?php
 
-class Secure extends CI_Controller {
-
-	//we collect the categories automatically with each load rather than for each function
-	//this just cuts the codebase down a bit
-	var $categories	= '';
-	
-	//this is so there will be a breadcrumb on every page even if it is blank
-	//the breadcrumbs currently suck. on a product page if you refresh, you lose the path
-	//will have to find a better way for these, but it's not a priority
-	var $breadcrumb	= '';	
-	
-	// determine whether to display gift card link on all cart pages
-	var $gift_cards_enabled = false; 
-	
-	//load all the pages into this variable so we can call it from all the methods
-	var $pages;
+class Secure extends Front_Controller {
 	
 	var $customer;
 	
@@ -23,34 +8,16 @@ class Secure extends CI_Controller {
 	{
 		parent::__construct();
 		
-		//check to see if they are on a secure URL, this will stop them from typing in the insecure url and
-		//attempting to force an insecure page.... why would someone do this? I dunnno....
 		force_ssl();
 		
-		$this->load->library('Go_cart');
-		$this->load->model(array('Page_model', 'Product_model', 'Option_model','location_model'));
-		$this->load->helper('form_helper');
+		$this->load->model(array('location_model'));
 		
 		$this->customer = $this->go_cart->customer();
-		
-		//fill up our categories variable
-		$this->categories =  $this->Category_model->get_categories_tierd(0);
-		$this->pages		= $this->Page_model->get_pages();
-		$gc_setting = $this->Settings_model->get_settings('gift_cards');
-		
-		if(!empty($gc_setting['enabled']) && $gc_setting['enabled']==1)
-		{
-			$this->gift_cards_enabled = true;
-		}
-		
-		//load the theme package
-		$this->load->add_package_path(APPPATH.'themes/'.$this->config->item('theme').'/');
 	}
 	
 	function index()
 	{
-		//we don't have a default landing page for secure
-		redirect('');
+		show_404();
 	}
 	
 	function login($ajax = false)
