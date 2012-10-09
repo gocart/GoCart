bb969eef383b5f07bec27830a29c240eb09caaae
Sean Downey <sean@considerweb.com>
2012-10-09 19:50:03 +0100

    Use the template library in the admin login

--- gocart/controllers/admin/login.php
+++ gocart/controllers/admin/login.php
@@ -10,6 +10,14 @@ class Login extends CI_Controller {
 		
 		$this->load->library('Auth');
 		$this->lang->load('login');
+		
+		$this->load->config('template');
+		$this->load->library('Template');
+		$this->template->set_theme('admin');
+		// create partials for the header and footer
+		$this->template->set('admin_url', site_url($this->config->item('admin_folder')).'/');
+		$this->template->set_partial('header', 'header');
+		$this->template->set_partial('footer', 'footer');
 	}
 
 	function index()
@@ -51,7 +59,7 @@ class Login extends CI_Controller {
 			}
 		}
 
-		$this->template->title($data['page_title'], config_item('company_name'));
+		$this->template->title(config_item('company_name'));
 		$this->template->build('login', $data);
 	}
 	
