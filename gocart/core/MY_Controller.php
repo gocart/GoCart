9f168c454e283a866f3ef516a90116241ba1c666
Noah Mormino <Programmer@Noahs-iMac.local>
2012-10-09 13:29:37 -0500

    added view and partial functions to MY_Controller.php

--- gocart/core/MY_Controller.php
+++ gocart/core/MY_Controller.php
@@ -70,7 +70,42 @@ class Front_Controller extends Base_Controller
 		$this->load->add_package_path(APPPATH.'themes/'.$this->config->item('theme').'/');
 	}
 	
+	/*
+	This works exactly like the regular $this->load->view()
+	The difference is it automatically pulls in a header and footer.
+	*/
+	function view($view, $vars = array(), $string=false)
+	{
+		if($string)
+		{
+			$result	 = $this->load->view('header', $vars, true);
+			$result	.= $this->load->view($view, $vars, true);
+			$result	.= $this->load->view('footer', $vars, true);
+			
+			return $result;
+		}
+		else
+		{
+			$this->load->view('header', $vars);
+			$this->load->view($view, $vars);
+			$this->load->view('footer', $vars);
+		}
+	}
 	
+	/*
+	This function simple calls $this->load->view()
+	*/
+	function partial($view, $vars = array(), $string=false)
+	{
+		if($string)
+		{
+			return $this->load->view($view, $vars, true);
+		}
+		else
+		{
+			$this->load->view($view, $vars);
+		}
+	}
 }
 
 class Admin_Controller extends Base_Controller 
