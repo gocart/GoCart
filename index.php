f78f46713d9575b83b1694c5d182de3fe711fd61
Noah Mormino <Programmer@Noahs-iMac.local>
2012-11-29 14:14:24 -0600

    Fixed install redirect for some servers

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 //if the gocart config file doesn't exist, the cart hasn't been installed.
 if(!file_exists(dirname($_SERVER['SCRIPT_FILENAME']).'/gocart/config/gocart.php'))
 {
-	$folder = (dirname($_SERVER['PHP_SELF'])=='index.php') ? dirname($_SERVER['PHP_SELF']) : '';
+	$folder = dirname($_SERVER['SCRIPT_NAME']);
 	$path	= rtrim($_SERVER['HTTP_HOST'].$folder, '/\\').'/';	
 	header('Location: http://'.$path.'install');
 	die;
