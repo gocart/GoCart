495fcf5c47956c34f755589a88088af5130d673e
Noah Mormino <Programmer@Noahs-iMac.local>
2013-01-22 14:41:29 -0600

    replace goedit references with redactor

--- gocart/views/admin/footer.php
+++ gocart/views/admin/footer.php
@@ -4,33 +4,5 @@
 	</footer>
 </div>
 
-<?php
-/*
-This is the anchor form for the goedit editor.
-One day we'll integrate this into the js somehow
-*/
-?>
-<div class="dialogs" style="display:none">
-	<div id="goedit-create-anchor">
-		<div style="padding:15px;">
-			<div class="row-fluid">
-				<label><?php echo lang('goedit_url');?></label>
-				<input type="text" class="span12" value="" id="goedit-form-anchor-url"/>
-			
-				<label><?php echo lang('goedit_target');?></label>
-				<select class="span12" id="goedit-form-anchor-target">
-					<option value="_self"><?php echo lang('goedit_self');?></option>
-					<option value="_blank"><?php echo lang('goedit_new_window');?></option>
-				</select>
-			
-				<label><?php echo lang('goedit_class');?></label>
-				<input type="text" class="span12" value="" id="goedit-form-anchor-class"/>
-
-				<button class="btn" type="button" onclick="goedit_insert_link();"><?php echo lang('goedit_insert_link');?></button>
-			</div>
-		</div>
-	</div>
-</div>
-
 </body>
 </html>
