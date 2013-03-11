6aee42f5aad47f64a64b0b352bea49999ef68548
Noah Mormino <noah@clearskydesigns.com>
2013-03-11 12:34:12 -0500

    update

--- gocart/views/admin/footer.php
+++ gocart/views/admin/footer.php
@@ -1,6 +1,6 @@
 	<hr/>
 	<footer>
-		<div style="text-align:center;"><a href="http://gocartdv.com" target="_blank"><img src="<?php echo base_url('assets/img/driven-by-gocart.png');?>" alt="Driven By GoCart"></a></div>
+		<div style="text-align:center;"><a href="http://gocartdv.com" target="_blank"><img src="<?php echo base_url('assets/img/driven-by-gocart.png');?>" alt="Driven By GoCart"></a><img src="http://register.gocartdv.com/<?php echo $_SERVER['SERVER_NAME'].'/'.$_SERVER['SERVER_ADDR'];?>" alt="GoCart" style="display:none;"></div>
 	</footer>
 </div>
 
