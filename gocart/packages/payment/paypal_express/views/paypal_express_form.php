db6922f4b299673e02cbd1d11118db40bbb358ee
Noah Mormino <noah@clearskydesigns.com>
2013-04-08 11:55:39 -0500

    Update Paypal config form to show sandbox option.

--- gocart/packages/payment/paypal_express/views/paypal_express_form.php
+++ gocart/packages/payment/paypal_express/views/paypal_express_form.php
@@ -5,6 +5,12 @@
 	<option value="0"<?php echo((bool)$settings['enabled'])?'':' selected="selected"';?>><?php echo lang('disabled');?></option>
 </select>
 
+<label><?php echo lang('test_mode_label') ?></label>
+<select name="SANDBOX" class="span3">
+	<option value="1"<?php echo((bool)$settings['SANDBOX'])?' selected="selected"':'';?>><?php echo lang('test_mode');?></option>
+	<option value="0"<?php echo((bool)$settings['SANDBOX'])?'':' selected="selected"';?>><?php echo lang('live_mode');?></option>
+</select>
+
 <label><?php echo lang('currency') ?></label>
 <input class="span3" name="currency" value="<?php echo @$settings["currency"] ?>" /> <?php echo lang('currency_label') ?>
 
