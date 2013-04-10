817c0188da6dbfe0d73b83aeb394a5dde74e243c
Noah Mormino <noah@clearskydesigns.com>
2013-04-10 14:41:14 -0500

    fix index to point to gocart rather than application folder.

--- index.php
+++ index.php
@@ -81,7 +81,7 @@ if (defined('ENVIRONMENT'))
  * NO TRAILING SLASH!
  *
  */
-	$application_folder = 'application';
+	$application_folder = 'gocart';
 
 /*
  * --------------------------------------------------------------------
