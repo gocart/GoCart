00a6c57ef148c41e2b43f034d974d375b2eda433
Gabriel Alack <gabe@clearskydesigns.com>
2013-10-28 21:40:08 -0500

    install changes

--- gocart/config/autoload.php
+++ gocart/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database');
+$autoload['libraries'] = array('database', 'auth', 'session');
 
 
 /*
