29e3217b7335520d4fefcbdf50ccd2f1f1b267f0
Noah Mormino <noah@noahmormino.com>
2013-11-15 15:29:37 -0600

    Remove the gocart.php config check

--- CI.php
+++ CI.php
@@ -1,14 +1,4 @@
 <?php
-
-//if the gocart config file doesn't exist, the cart hasn't been installed.
-if(!file_exists(dirname($_SERVER['SCRIPT_FILENAME']).'/gocart/config/gocart.php'))
-{
-	$folder = dirname($_SERVER['SCRIPT_NAME']);
-	$path	= rtrim($_SERVER['HTTP_HOST'].$folder, '/\\').'/';	
-	header('Location: http://'.$path.'install');
-	die;
-}
-
 /*
  *---------------------------------------------------------------
  * APPLICATION ENVIRONMENT
