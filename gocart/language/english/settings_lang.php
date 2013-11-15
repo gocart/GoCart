a4f3190ed49e76273617d5a95a38249c85ae0d26
Noah Mormino <noah@noahmormino.com>
2013-11-15 15:47:41 -0600

    updated setting language

--- gocart/language/english/settings_lang.php
+++ gocart/language/english/settings_lang.php
@@ -4,27 +4,69 @@ US English
 Admin Settings Language
 ******************************************/
 
-$lang['settings']					= 'Settings';
-$lang['message_name']				= 'Message Name';
-$lang['subject']					= 'Subject';
-$lang['message_content']			= 'Message Content';
-$lang['canned_messages']			= 'Canned Messages';
-$lang['canned_message_form']		= 'Canned Message Form';
-$lang['settings_updated']			= '"%s" settings have been updated.';
-$lang['payment_settings']			= '"%s" payment settings.';
-$lang['shipping_settings']			= '"%s" shipping settings.';
-$lang['payment_settings_title']		= 'Payment Settings';
-$lang['shipping_settings_title']	= 'Shipping Settings';
-$lang['add_canned_message']			= 'Add Canned Message';
-$lang['payment_modules']			= "Payment Modules";
-$lang['shipping_modules']			= "Shipping Modules";
-$lang['uninstall']					= 'Uninstall';
-$lang['install']					= 'Install';
-$lang['confirm_are_you_sure']		= 'Are you sure you want to do this?';
-$lang['message_saved_message']		= 'The message has been saved.';
-$lang['message_deleted_message']	= 'The message has been deleted.';
+$lang['settings'] = 'Settings';
+$lang['message_name'] = 'Message Name';
+$lang['subject'] = 'Subject';
+$lang['message_content'] = 'Message Content';
+$lang['canned_messages'] = 'Canned Messages';
+$lang['canned_message_form'] = 'Canned Message Form';
+$lang['settings_updated'] = '"%s" settings have been updated.';
+$lang['payment_settings'] = '"%s" payment settings.';
+$lang['shipping_settings'] = '"%s" shipping settings.';
+$lang['payment_settings_title'] = 'Payment Settings';
+$lang['shipping_settings_title'] = 'Shipping Settings';
+$lang['add_canned_message'] = 'Add Canned Message';
+$lang['payment_modules'] = "Payment Modules";
+$lang['shipping_modules'] = "Shipping Modules";
+$lang['uninstall'] = 'Uninstall';
+$lang['install'] = 'Install';
+$lang['confirm_are_you_sure'] = 'Are you sure you want to do this?';
+$lang['message_saved_message'] = 'The message has been saved.';
+$lang['message_deleted_message'] = 'The message has been deleted.';
+$lang['config_updated_message'] = 'GoCart configuration has been updated.';
+$lang['status_exists_error'] = 'The requested status already exists.';
+$lang['confirm_delete_order_status'] = 'Are you sure you want to remove this order status?';
 
-$lang['save_canned_message']		= 'Save Message';
-$lang['tab_canned_info']			= 'Message Info';
-$lang['label_canned_name']			= 'Name';
-$lang['label_canned_subject']		= 'Subject';
+$lang['save_canned_message'] = 'Save Message';
+$lang['tab_canned_info'] = 'Message Info';
+$lang['label_canned_name'] = 'Name';
+$lang['label_canned_subject'] = 'Subject';
+
+//gocart config titles
+$lang['shop_details'] = 'Shop Details';
+$lang['company_name'] = 'Company Name';
+$lang['ship_from_address'] = 'Ship From Address';
+$lang['cart_email'] = 'Cart Email';
+$lang['locale_currency'] = 'Locale &amp; Currency';
+$lang['security'] = 'Security';
+$lang['package_details'] = 'Package Details';
+$lang['order_inventory'] = 'Orders &amp; Inventory';
+$lang['tax_settings'] = 'Tax Settings';
+
+//config terms
+$lang['theme'] = 'GoCart Theme';
+$lang['ssl_support'] = 'Use SSL';
+$lang['company_name'] = 'Shop Name';
+$lang['address1'] = 'Address';
+$lang['address2'] = 'Address 2';
+$lang['country'] = 'Country';
+$lang['state'] = 'State / Province';
+$lang['zip'] = 'Zip / Postcode';
+$lang['city'] = 'City';
+$lang['locale'] = 'Locale';
+$lang['currency'] = 'Currency';
+
+$lang['admin_folder'] = 'Admin Folder';
+$lang['require_login'] = 'Require login to checkout.';
+$lang['new_customer_status'] = 'New customers are automatically activated.';
+$lang['weight_unit'] = 'Weight Unit (LB, OZ, KG, etc.)';
+$lang['dimension_unit'] = 'Dimensional Unit (FT, IN, CM, etc.)';
+$lang['order_status'] = 'Default Order Status';
+$lang['order_statuses'] = 'Order Status Options';
+$lang['status_name'] = 'Status Name';
+$lang['inventory_enabled'] = 'Enable Inventory Tracking';
+$lang['allow_os_purchase'] = 'Allow customers to buy items that are out of stock';
+$lang['tax_shipping'] = 'Charge tax on shipping rates';
+$lang['tax_address'] = 'Base sales tax of which address?';
+$lang['shipping_address'] = 'Shipping Address';
+$lang['billing_address'] = 'Billing Address';
