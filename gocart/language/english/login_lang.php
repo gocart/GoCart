57a10e06ce8be0ce2eaaa80d57d5cc47f961a860
Noah Mormino <noah@noahmormino.com>
2013-11-15 15:54:29 -0600

    upload email verbiage to username verbiage on login page.

--- gocart/language/english/login_lang.php
+++ gocart/language/english/login_lang.php
@@ -4,10 +4,11 @@ US English
 Admin login Language
 ******************************************/
 
-$lang['error_authentication_failed']	= 'Authentication Failed!';
-$lang['message_logged_out']				= 'You have been logged out.';
-$lang['gocart_login']					= 'GoCart Login';
-$lang['login']							= 'Login';
-$lang['password']						= 'Password';
-$lang['email']							= 'Email';
-$lang['stay_logged_in']					= 'Keep me logged in';
+$lang['error_authentication_failed'] = 'Authentication Failed!';
+$lang['message_logged_out'] = 'You have been logged out.';
+$lang['gocart_login'] = 'GoCart Login';
+$lang['login'] = 'Login';
+$lang['password'] = 'Password';
+$lang['email'] = 'Email';
+$lang['username'] = 'Username';
+$lang['stay_logged_in'] = 'Keep me logged in';
