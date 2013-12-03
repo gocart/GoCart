7f4de061a5dcb37d46556ca2b9784dbbbbc9bebc
Noah Mormino <noah@noahmormino.com>
2013-12-03 13:28:23 -0600

    Add viewport meta tag

--- gocart/themes/default/views/header.php
+++ gocart/themes/default/views/header.php
@@ -2,6 +2,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <title><?php echo (!empty($seo_title)) ? $seo_title .' - ' : ''; echo $this->config->item('company_name'); ?></title>
 
 
