ff568337fc93ff98221aef122e9595250bfc7002
Noah Mormino <noah@noahmormino.com>
2013-12-03 16:21:50 -0600

    https

--- gocart/views/admin/footer.php
+++ gocart/views/admin/footer.php
@@ -1,6 +1,6 @@
     <hr/>
     <footer>
-        <div style="text-align:center;"><a href="http://gocartdv.com" target="_blank"><img src="<?php echo base_url('assets/img/drivenByGoCart.svg');?>" alt="Driven By GoCart" style="width:120px;"></a><img src="http://register.gocartdv.com/<?php echo $_SERVER['SERVER_NAME'].'/'.$_SERVER['SERVER_ADDR'];?>" alt="GoCart" style="display:none;"></div>
+        <div style="text-align:center;"><a href="http://gocartdv.com" target="_blank"><img src="<?php echo base_url('assets/img/drivenByGoCart.svg');?>" alt="Driven By GoCart" style="width:120px;"></a><img src="https://register.gocartdv.com/<?php echo $_SERVER['SERVER_NAME'].'/'.$_SERVER['SERVER_ADDR'];?>" alt="GoCart" style="display:none;"></div>
     </footer>
 </div>
 
