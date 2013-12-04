d86ac6ef82c176fb33faff9f6b082774fd1bec32
Noah Mormino <noah@noahmormino.com>
2013-12-04 09:14:21 -0600

    fix logo

--- gocart/themes/default/views/footer.php
+++ gocart/themes/default/views/footer.php
@@ -1,10 +1,9 @@
-	<footer class="footer">
+    <footer class="footer">
 
-		<div style="text-align:center;"><a href="http://gocartdv.com" target="_blank"><img src="<?php echo base_url('assets/img/driven-by-gocart.png');?>" alt="Driven By GoCart"></a></div>
-		
-	</footer>
+        <div style="text-align:center;"><a href="http://gocartdv.com" target="_blank"><img src="<?php echo base_url('assets/img/drivenByGoCart.svg');?>" alt="Driven By GoCart"></a></div>
+        
+    </footer>
 </div>
-
 
 </body>
 </html>
